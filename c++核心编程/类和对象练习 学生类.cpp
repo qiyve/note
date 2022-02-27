7e06e33477b09377973f22197924d7c1cc2ce1f5
new_dev <1872665996@qq.com>
2022-02-27 12:37:47 +0800

    Update 类和对象练习 学生类.cpp

--- c++核心编程/类和对象练习 学生类.cpp
+++ c++核心编程/类和对象练习 学生类.cpp
@@ -14,6 +14,11 @@ class Student
 	//访问权限
 public://公共权限
 
+	//类中的属性和行为，我们统一称为 成员
+	//属性  成员属性  成员变量
+	//行为  成员函数  成员方法
+
+
 	//属性
 	string m_Name;//姓名
 	int m_ID;//学号
@@ -24,14 +29,27 @@ public://公共权限
 	{
 		cout << "姓名：" << m_Name << "学号：" << m_ID << endl;
 	}
+	//给姓名赋值
+	void setName(string name)
+	{
+		m_Name = name;
+	}
+	//给学号赋值
+	void setID(int id)
+	{
+		m_ID = id;
+	}
 };
 int main()
 {
 	//创建一个具体学生
 	Student s1;
 	//给s1对象 进行属性赋值
-	s1.m_Name = "阿三";
-	s1.m_ID = 1;
+	
+	//s1.m_Name = "阿三";
+	s1.setName("阿三");
+	//s1.m_ID=1;
+	s1.setID(1);
 
 	//显示学生信息
 	s1.showStudent();
