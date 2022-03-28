faea3eb9b0750444bc1c1dd50d4cfcad13b6df84
new_dev <1872665996@qq.com>
2022-03-28 20:53:49 +0800

    Create Student.cpp

---
+++ 案例合集/机房预约系统/Student.cpp
@@ -0,0 +1,50 @@
+#include"Student.h"
+
+//默认构造
+Student::Student()
+{
+
+
+}
+
+//有参构造  参数：学号 姓名 密码
+Student::Student(int id, string name, string pwd)
+{
+
+
+}
+
+//菜单界面
+void Student::operMenu()
+{
+
+
+}
+
+//申请预约
+void Student::applyOrder()
+{
+
+
+}
+
+//查看自身预约
+void Student::showMyOrder()
+{
+
+
+}
+
+//查看所有预约
+void Student::showAllOrder()
+{
+
+
+}
+
+//取消预约
+void Student::cancelOrder()
+{
+
+
+}
