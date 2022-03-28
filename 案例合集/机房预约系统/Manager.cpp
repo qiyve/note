8faedaf7139f0cfae82b2d8f907d7b0dba75053f
new_dev <1872665996@qq.com>
2022-03-28 20:53:44 +0800

    Create Manager.cpp

---
+++ 案例合集/机房预约系统/Manager.cpp
@@ -0,0 +1,43 @@
+#include"Manager.h"
+
+//默认构造
+Manager::Manager()
+{
+
+}
+
+//有参构造
+Manager::Manager(string name, string pwd)
+{
+
+}
+
+//菜单界面
+void Manager::operMenu()
+{
+
+}
+
+//添加账号
+void Manager::addPerson()
+{
+
+}
+
+//查看账号
+void Manager::showPerson()
+{
+
+}
+
+//查看机房信息
+void Manager::showComputer()
+{
+
+}
+
+//清空机房数据
+void Manager::cleanFile()
+{
+
+}
