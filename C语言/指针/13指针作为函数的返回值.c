1c4fa19662ec0f5f4b6890522d77bfc047fd4e3e
new_dev <1872665996@qq.com>
2022-04-11 22:18:46 +0800

    Update 13指针作为函数的返回值.c

--- C语言/指针/13指针作为函数的返回值.c
+++ C语言/指针/13指针作为函数的返回值.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-char* my_strchar01(char* str, char ch)
+char* my_strchar01(char* str, char ch)//ch´ú±í×Ö·û
 {
 	int i = 0;
+	//str[i]!='\0'
 	while (str[i])
 	{
 		if (str[i] == ch)
@@ -14,14 +15,13 @@ char* my_strchar01(char* str, char ch)
 
 	return NULL;
 }
+
 char* my_strchar(char* str, char ch)
 {
 	while (*str)
 	{
 		if (*str == ch)
-		{
 			return str;
-		}
 		str++;
 	}
 	return NULL;
