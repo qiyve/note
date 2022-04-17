e0680253ef2616057fddbe1f00dc62f673ff77f8
new_dev <1872665996@qq.com>
2022-04-17 15:52:12 +0800

    Update 03LinKist.c

--- C++数据结构/03LinKist.c
+++ C++数据结构/03LinKist.c
@@ -44,12 +44,54 @@ void Insert_LinkList(LinkList* list, int pos, LinkNode* data)
 //删除
 void Remove_LinkList(LinkList* list, int pos)
 {
+	if (list == NULL)
+	{
+		return;
+	}
+	if (pos<0 || pos>=list->size)
+	{
+		return;
+	}
+	
+	//查找位置
+	LinkNode* pCurrent = &(list->head);
+	for (int i = 0; i < pos; i++)
+	{
+		pCurrent = pCurrent->next;
+	}
 
+	pCurrent->next = pCurrent->next->next;
+
+	list->size--;
 }
 //查找
-int Find_LinkList(LinkList* list, LinkList* data)
+int Find_LinkList(LinkList* list, LinkNode* data, COMPARENODE compare)
 {
-	return 0;
+	if (list == NULL)
+	{
+		return -1;
+	}
+	if (data == NULL)
+	{
+		return -1;
+	}
+
+	//辅助指针变量
+	LinkNode* pCurrent = list->head.next;
+	int index = 0;
+	while (pCurrent != NULL)
+	{
+		if (compare(pCurrent, data) == 0)
+		{
+			break;
+		}
+
+		pCurrent = pCurrent->next;
+		index++;
+	}
+
+
+	return index;
 }
 //返回链表大小
 int Size_LinkList(LinkList* list)
